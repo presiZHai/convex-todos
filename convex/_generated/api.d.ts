f530107f60d59200f54b045f6745248c8e0c4732
Olawale Ogundeji <abiodungndj@gmail.com>
2024-09-07 18:15:11 +0100

    Link users to todos

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as functions from "../functions.js";
+import type * as helpers from "../helpers.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,6 +26,7 @@ import type * as functions from "../functions.js";
  */
 declare const fullApi: ApiFromModules<{
   functions: typeof functions;
+  helpers: typeof helpers;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
--- convex/functions.ts
+++ convex/functions.ts
@@ -1,11 +1,15 @@
 import { v } from "convex/values";
 import  { mutation, query } from "./_generated/server"
+import { requireUser } from "./helpers";
 
 export const listTodods = query({
     handler : async (ctx) => {
-        return await ctx.db.query("todos").collect();
+        const user = await requireUser(ctx);
+        return await ctx.db.query("todos")
+            .withIndex("by_user_id", q => q.eq("userId", user.tokenIdentifier))
+            .collect();
     }
-})
+});
 
 export const createTodo = mutation({
     args: {
@@ -13,10 +17,12 @@ export const createTodo = mutation({
         description: v.string(),
     },
     handler : async (ctx, args) => {
+        const user = await requireUser(ctx);
         await ctx.db.insert("todos", {
             title: args.title,
             description: args.description,
-            completed: false
+            completed: false,
+            userId: user.tokenIdentifier,
         });
     }
 });
@@ -27,6 +33,11 @@ export const updateTodo = mutation({
         completed: v.boolean(),
     },
     handler : async (ctx, args) => {
+        const user = await requireUser(ctx);
+        const todo = await ctx.db.get(args.id);
+        if (todo?.userId !== user.tokenIdentifier) {
+            throw new Error("Unauthorized");
+        }
         await ctx.db.patch(args.id, {
             completed: args.completed
         });
@@ -38,6 +49,11 @@ export const deleteTodo = mutation({
         id: v.id("todos"),
     },
     handler : async (ctx, args) => {
+        const user = await requireUser(ctx);
+        const todo = await ctx.db.get(args.id);
+        if (todo?.userId !== user.tokenIdentifier) {
+            throw new Error("Unauthorized");
+        }
         await ctx.db.delete(args.id);
     }
 });
