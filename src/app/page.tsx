83746324f09d82610f55561cf7220913775b9901
Olawale Ogundeji <abiodungndj@gmail.com>
2024-09-07 12:39:40 +0100

    Add clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { NewToDoForm } from "./_components/new-todo-form";
 import { ToDoList } from "./_components/to-do-list";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 
-
 export default function Home() { 
   return (
     <div className="max-w-screen-md mx-auto p-4 space-y-4">
